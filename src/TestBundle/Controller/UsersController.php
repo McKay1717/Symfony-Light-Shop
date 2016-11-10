357810e7958f14a2e3e533d80ce6f2b74b72efea
McKay1717 <nicolas1706ni@gmail.com>
2016-11-10 21:00:26 +0100

    Fix UserController (Bad Models name)

--- src/TestBundle/Controller/UsersController.php
+++ src/TestBundle/Controller/UsersController.php
@@ -39,7 +39,7 @@ class UsersController extends Controller
      */
     public function newAction(Request $request)
     {
-        $user = new User();
+        $user = new Users();
         $form = $this->createForm('TestBundle\Form\UsersType', $user);
         $form->handleRequest($request);
 
