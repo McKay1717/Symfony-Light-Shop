562b7b787b623419a0bd1bc12b750cfd42390318
McKay1717 <nicolas1706ni@gmail.com>
2016-11-10 22:17:32 +0100

    Add Menu bar

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -5,7 +5,7 @@ class AppKernel extends Kernel {
 	public function registerBundles() {
 		$bundles = [ 
 				new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-				new Neimheadh\Bundle\BootstrapBundle\NeimheadhBootstrapBundle(),
+				new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
 				new Symfony\Bundle\FrameworkBundle\FrameworkBundle (),
 				new Symfony\Bundle\SecurityBundle\SecurityBundle (),
 				new Symfony\Bundle\TwigBundle\TwigBundle (),
