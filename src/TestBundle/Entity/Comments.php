afa8d6eb0b1e56433a6e48bb66795186971cb0c4
McKay1717 <nicolas1706ni@gmail.com>
2016-12-12 11:14:32 +0100

    Add crud for comment

---
+++ src/TestBundle/Entity/Comments.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace TestBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Comments
+ *
+ * @ORM\Table(name="comments", indexes={@ORM\Index(name="fk_user_id", columns={"user_id"}), @ORM\Index(name="fk_product_id", columns={"product_id"})})
+ * @ORM\Entity
+ */
+class Comments
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="string", length=255, nullable=false)
+     */
+    private $content;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime", nullable=false)
+     */
+    private $date = 'CURRENT_TIMESTAMP';
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var \TestBundle\Entity\Produits
+     *
+     * @ORM\ManyToOne(targetEntity="TestBundle\Entity\Produits")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="product_id", referencedColumnName="id")
+     * })
+     */
+    private $product;
+
+    /**
+     * @var \TestBundle\Entity\Users
+     *
+     * @ORM\ManyToOne(targetEntity="TestBundle\Entity\Users")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     * })
+     */
+    private $user;
+
+
+}
+
---
+++ src/TestBundle/Form/CommentsType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace TestBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentsType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('content')->add('date')->add('product')->add('user')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'TestBundle\Entity\Comments'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'testbundle_comments';
+    }
+
+
+}
